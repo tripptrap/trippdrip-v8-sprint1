97548565f0ee049f4aaac382e63154f72fd872f7
tripptrap <42554192+tripptrap@users.noreply.github.com>
2025-11-11 19:34:26 -0500

    Fix Stripe API version

--- app/api/cron/process-twilio-usage/route.ts
+++ app/api/cron/process-twilio-usage/route.ts
@@ -7,7 +7,7 @@ import { createClient } from '@supabase/supabase-js';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-12-18.acacia',
+  apiVersion: '2025-10-29.clover',
 });
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
